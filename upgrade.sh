28adc8ec77b77bfed23f1dd128a805f3d6ac2fad
Ronnakit PC <ronnakit_b@cmu.ac.th>
2021-07-28 02:15:52 +0700

    เพิ่ม version

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
 read -p 'namespace : ' namespace
 read -p 'version : ' version
 
-helm list -n $namespace | grep acc | awk '{print $1 " ./$version"}' | xargs -L1 helm upgrade -n $namespace ./0.5
+helm list -n $namespace | grep acc | awk '{print $1 " ./$version"}' | xargs -L1 helm upgrade -n $namespace
